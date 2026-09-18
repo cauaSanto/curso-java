ffebf2aacea59015bdc5b029b75cbe335041435d
caua oliveira <cauaoliveira.mg@gmail.com>
2026-09-18 11:35:57 -0300

    2° aula

---
+++ 02-cadastroCliente/src/br.com.rpires/domain/IClienteDAO.java
@@ -0,0 +1,11 @@
+import java.util.Collection;
+
+
+public interface IClienteDAO {
+
+    public boolean cadastrar(Cliente cliente);
+    public  void excluir(Long cpf);
+    public void alterar(Cliente cliente);
+    public Cliente consultar(Long cpf);
+    public Collection<Cliente> buscarTodos();
+ }
