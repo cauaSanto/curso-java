1346c77f7653948c530f3a363e66b6e562995543
caua oliveira <cauaoliveira.mg@gmail.com>
2026-09-19 15:57:22 -0300

    colocando aula correta

---
+++ 03_singleton/TesteSingleton.java
@@ -0,0 +1,8 @@
+public class TesteSingleton {
+   
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+
+        System.out.println(singleton);
+    }
+}
